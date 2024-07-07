d3c42c62dad50e47efcacc7ad346c48d5df5eaa3
Brais <braisfv22@gmail.com>
2024-07-07 21:32:28 +0200

    updated di

--- composeApp/src/commonMain/kotlin/com/braiso_22/bichota_timer/tasks/di/TaskModule.kt
+++ composeApp/src/commonMain/kotlin/com/braiso_22/bichota_timer/tasks/di/TaskModule.kt
@@ -2,11 +2,15 @@ package com.braiso_22.bichota_timer.tasks.di
 
 import com.braiso_22.bichota_timer.tasks.data.TaskRepositoryMock
 import com.braiso_22.bichota_timer.tasks.domain.TaskRepository
+import com.braiso_22.bichota_timer.tasks.domain.usecases.GetExecutionById
+import com.braiso_22.bichota_timer.tasks.domain.usecases.GetSegmentById
+import com.braiso_22.bichota_timer.tasks.domain.usecases.GetTaskById
 import com.braiso_22.bichota_timer.tasks.domain.usecases.UpsertExecution
 import com.braiso_22.bichota_timer.tasks.domain.usecases.UpsertTask
 import com.braiso_22.bichota_timer.tasks.domain.usecases.GetTasksWithAllExecutionsByUser
 import com.braiso_22.bichota_timer.tasks.domain.usecases.GetTasksWithExecutionsInDateRange
 import com.braiso_22.bichota_timer.tasks.domain.usecases.GetWorkedHoursOfTasks
+import com.braiso_22.bichota_timer.tasks.domain.usecases.UpsertSegment
 import com.braiso_22.bichota_timer.tasks.presentation.add_task.AddTaskViewModel
 import com.braiso_22.bichota_timer.tasks.presentation.my_day.MyDayViewModel
 import org.koin.compose.viewmodel.dsl.viewModelOf
@@ -19,9 +23,12 @@ val taskModule = module {
     singleOf(::GetTasksWithAllExecutionsByUser).bind()
     singleOf(::GetTasksWithExecutionsInDateRange).bind()
     singleOf(::GetWorkedHoursOfTasks).bind()
+    singleOf(::GetTaskById).bind()
+    singleOf(::GetSegmentById).bind()
+    singleOf(::GetExecutionById).bind()
     singleOf(::UpsertTask).bind()
     singleOf(::UpsertExecution).bind()
-
+    singleOf(::UpsertSegment).bind()
 
     viewModelOf(::MyDayViewModel)
     viewModelOf(::AddTaskViewModel)
